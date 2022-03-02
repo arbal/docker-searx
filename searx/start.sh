fcdbc7d4e81a908b5e9c1956e394262023787231
Oscar <silkky@silkky.cloud>
2022-03-02 16:20:45 +1100

    Add SearXNG start script
    Signed-off-by: Oscar <silkky@silkky.cloud>

--- searx/start.sh
+++ searx/start.sh
@@ -1,2 +1,26 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+if [[ -v HOSTNAME ]]; then
+  sed -i "s@ENV_HOSTNAME@$HOSTNAME@g" /data/settings.yml
+else
+  echo "No hostname!"
+  exit 1;
+fi
+
+if [[ -v SECRET_KEY ]]; then
+  sed -i "s@ENV_SECRET_KEY@$SECRET_KEY@g" /data/settings.yml
+else
+  echo "No secret key!"
+  exit 1;
+fi
+
+if [[ -v MORTY_KEY ]]; then
+  sed -i "s@ENV_MORTY_KEY@$MORTY_KEY@g" /data/settings.yml
+else
+  echo "No morty key!"
+  exit 1;
+fi
+
+sleep 2s
+
+exec uwsgi --master --http-socket "0.0.0.0:8080" "/data/uwsgi.ini"
